d0d50bb2dc897e80d27168bde166342f72e491df
ulfatun_amanah <ulfaamana@gmail.com>
2022-01-18 22:48:15 +0700

    revisi merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ForgotPassword from './container/pages/ForgotPassword';
+import Home from './container/pages/Home/Home';
 import Login from './container/pages/Login';
-import PlayGame from './container/pages/PlayGame';
+// import PlayGame from './container/pages/PlayGame';
 import Register from './container/pages/Register';
 import ResetPassword from './container/pages/ResetPassword';
 
@@ -10,11 +11,12 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/play-game" element={<PlayGame />} />
+        {/* <Route path="/play-game" element={<PlayGame />} /> */}
       </Routes>
     </Router>
   );
