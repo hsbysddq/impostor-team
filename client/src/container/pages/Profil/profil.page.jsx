669924efc2ca903b88057b5a9530abf87e8ffbf4
hsbysddq <hsby15sddq@gmail.com>
2022-01-21 19:05:49 +0700

    resolve

--- client/src/container/pages/Profil/profil.page.jsx
+++ client/src/container/pages/Profil/profil.page.jsx
@@ -33,7 +33,7 @@ const Profil = () => {
                                 <Card.Body>
                                     <Card.Title>Special title treatment</Card.Title>
                                     <Card.Text>
-                                    With supporting text below as a natural lead-in to additional content.
+                                    With supporting text below as  natural lead-in to additional content.
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
