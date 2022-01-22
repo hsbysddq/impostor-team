9a35d8f8343530f7aea7640fcd32468e28a0b9fa
saufiroja <saufiroja08@gmail.com>
2022-01-22 18:57:06 +0800

    added game

---
+++ client/src/components/Game/Wrapper.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const WrapperStyled = styled.div`
+  max-width: 700px;
+  margin: auto;
+`;
+
+function Wrapper({ children }) {
+  return <WrapperStyled>{children}</WrapperStyled>;
+}
+
+export default Wrapper;
