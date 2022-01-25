93076e9ba0fc35fa6001c6502ead57fb0daf7ab2
saufiroja <saufiroja08@gmail.com>
2022-01-25 13:14:06 +0800

    delete client

--- client/public/assets/index.jsx
+++
@@ -1,8 +0,0 @@
-import Game1 from './img/game1.png';
-import Game2 from './img/game2.png';
-import Game3 from './img/game3.png';
-import Game4 from './img/game4.png';
-import Game5 from './img/game5.png';
-import Game6 from './img/game6.png';
-
-export {Game1, Game2, Game3, Game4, Game5, Game6}
--- server/database/models/sequelize.js
+++ server/database/models/sequelize.js
@@ -8,6 +8,18 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   dialect: 'postgres',
   timezone: '+08:00',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  pool: {
+    max: 100,
+    min: 0,
+    idle: 200000,
+    acquire: 1000000,
+  },
 });
 
 sequelize.authenticate().then(() => {
